4b68d00ccfc4b6f8aaa86dfe688fc4ea94be6ed2
abruno36 <abruno36@gmail.com>
2024-01-19 08:51:17 -0300

    Atualização APK - 19120851

--- android/app/src/main/java/com/anonymous/pizzaria/MainActivity.java
+++ android/app/src/main/java/com/anonymous/pizzaria/MainActivity.java
@@ -1,4 +1,4 @@
-package com.anonymous.pizzariaMobile;
+package com.anonymous.pizzaria;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/anonymous/pizzaria/MainApplication.java
+++ android/app/src/main/java/com/anonymous/pizzaria/MainApplication.java
@@ -1,4 +1,4 @@
-package com.anonymous.pizzariaMobile;
+package com.anonymous.pizzaria;
 
 import android.app.Application;
 import android.content.res.Configuration;
